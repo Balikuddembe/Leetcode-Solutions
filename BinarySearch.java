f7e4995a70db2c22abb966788037f8faf7a73653
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-08-21 12:30:02 +0300

    Remove the main method

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,5 @@
-public class BinarySearch {public static boolean binarySearch(int[]orderedArray, int targetValue) {
+public class BinarySearch {
+    public static boolean binarySearch(int[]orderedArray, int targetValue) {
     int left = 0;
     int right = orderedArray.length-1;
     while (left<=right) {
@@ -14,12 +15,5 @@ public class BinarySearch {public static boolean binarySearch(int[]orderedArray,
         }
     }
     return false;
-}
-    public static void main(String[] args) {
-        int orderedArray[] = {1,2,3,4,5,6,7,8,9};
-        int targetValue = 0;
-        boolean result = binarySearch(orderedArray,targetValue);
-
-        System.out.println(result);
     }
 }
