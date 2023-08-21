f53628d275d41d121d8e4bc8754154e0c30811a0
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-08-21 18:45:54 +0300

    two sum coding challenge

---
+++ TwoSum.java
@@ -0,0 +1,22 @@
+class TwoSum {
+  public static int[] twoSum(int[] nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      int numToFind = target - nums[i];
+      for (int j =i+1; j < nums.length; j++) {
+        if (nums[j] == numToFind) {
+          return new int[] { i, j };
+        }
+      }
+    }
+    return new int[0];
+  }
+
+  public static void main(String[] args) {
+    int[] nums = { 3, 2, 4 };
+    int target = 6;
+    int[] result = twoSum(nums, target);
+    for (int num : result) {
+      System.out.println(num);
+    }
+  }
+}
