a5c51b4e803cb725727f628447f6610be04a9c83
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-08-26 21:49:10 +0300

    IndexOfFirstOccurrence challenge

--- IndexOfOccurrence.java
+++ IndexOfOccurrence.java
@@ -2,7 +2,7 @@ class IndexOfOccurrence {
     public static int strStr(String haystack, String needle) {
         int hayLength = haystack.length();
         int needlLength = needle.length();
-        for(int i=0;i<hayLength;i++) {
+        for(int i=0;i<=hayLength-needlLength;i++) {
             int j;
             for(j=0;j<needlLength;j++) {
                 if(haystack.charAt(i+j) !=needle.charAt(j)) {
