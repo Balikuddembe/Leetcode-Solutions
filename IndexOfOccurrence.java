e42e5c3dfd0d9839327f51a33804a2fd002510a6
Emmanuel Balikuddembe <balixemma@gmail.com>
2023-08-26 21:38:13 +0300

    IndexOfFirstOccurrence challenge

---
+++ IndexOfOccurrence.java
@@ -0,0 +1,18 @@
+class IndexOfOccurrence {
+    public static int strStr(String haystack, String needle) {
+        int hayLength = haystack.length();
+        int needlLength = needle.length();
+        for(int i=0;i<hayLength;i++) {
+            int j;
+            for(j=0;j<needlLength;j++) {
+                if(haystack.charAt(i+j) !=needle.charAt(j)) {
+                    break;
+                }
+            }
+            if(j==needlLength) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
